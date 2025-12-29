c2433cd7d9700a3f13e009f6572a7b26ab67e132
1392-github <ujo1033@gmail.com>
2025-12-29 21:09:15 +0900

    fix(65): 편집 창 불필요한 css제거

--- data.py
+++ data.py
@@ -9,7 +9,7 @@ class SpecialFunction:
     perm: str = "any"
     urlfor: bool = True
 
-version = (64, 0)
+version = (65, 0)
 default_config = {
     "version": str(version[0]),
     "version2": str(version[1]),
