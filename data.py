0f299fc44b1fce71150163da2bc09c65ca885f4d
1392-github <ujo1033@gmail.com>
2026-01-04 20:42:39 +0900

    fix(67-3): modal 미디어 쿼리를 main.css로 이전

--- data.py
+++ data.py
@@ -9,7 +9,7 @@ class SpecialFunction:
     perm: str = "any"
     urlfor: bool = True
 
-version = (67, 2)
+version = (67, 3)
 default_config = {
     "version": str(version[0]),
     "version2": str(version[1]),
